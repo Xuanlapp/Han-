1ea1ea3b250418d8bbe6eba6d32f684cd03d9927
Xuanlapp <xuanlap250203@gmail.com>
2026-03-27 14:30:51 +0700

    save current work

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -265,7 +265,7 @@ export const regenerateRedesignedObject = async (
     parts.push({ text: prompt });
 
     const response = await ai.models.generateContent({
-      model: 'gemini-2.5-flash-image',
+      model: 'gemini-3-pro-image-preview',
       contents: { parts },
       config: {
         imageConfig: {
@@ -340,7 +340,7 @@ export const generateSimilarObject = async (
     `;
 
     const response = await ai.models.generateContent({
-      model: 'gemini-2.5-flash-image',
+      model: 'gemini-3-pro-image-preview',
       contents: {
         parts: [
             { text: prompt },
